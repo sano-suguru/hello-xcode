7645ad372ec8119899c673a74744ba12c2a91740
Sano Suguru <sano@mbp17.local>
2018-10-08 16:13:58 +0900

    Initial Commit

---
+++ SwiftStart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftStart
+//
+//  Created by Sano Suguru on 2018/10/08.
+//  Copyright © 2018年 Sano Suguru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
